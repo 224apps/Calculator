3571e5b4768b0948aec8db0229843807c08c2771
Abdoulaye Diallo <shotone@icloud.com>
2014-08-18 10:32:29 -0400

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by A. Diallo on 8/18/14.
+//  Copyright (c) 2014 Remote Tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
